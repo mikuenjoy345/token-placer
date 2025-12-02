04d86c31ac75ea0938970e0a649bceeb243ceab5
jjakke <jjakke@dev.jjakke.com>
2025-12-02 14:35:17 -0500

    GIT THIS

---
+++ token-placer.pl
@@ -0,0 +1,110 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite -signatures;
+use Mojo::Util qw(md5_sum);
+use Mojo::Cache;
+use JSON qw(from_json);
+{
+	package TieAlphabet;
+	use strict;
+
+	sub TIEARRAY {
+		my ($pkg) = @_;
+		bless [qw(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)], $pkg;
+	}
+
+	sub FETCH {
+		my ($obj, $index) = @_;
+		return 'too big' if $index > (26*26+26); # only doing a-zz, not infinity.
+		$index < 26
+			? return $obj->[$index]
+			: return $obj->[ (int $index/26) - 1 ] . $obj->[ $index % 26 ]
+	}
+
+	sub FETCHSIZE {
+		return 26*26+26;
+	}
+	1;
+}
+
+my $cache = Mojo::Cache->new(max_keys => 10);
+my @alphabet_array;
+tie @alphabet_array, 'TieAlphabet';
+
+get '/' => sub ($c) {
+  $c->render(template => 'index');
+};
+
+any '/field' => sub ($c) {
+	$c->render(template => 'field');
+};
+
+get '/create_table' => sub ($c) {
+	return $c->render(template => 'create_table_form') unless $c->param('cols');
+	my $values = $c->req->params->to_hash;
+	$c->stash(cols => $values->{cols});
+	$c->stash(rows => $values->{rows});
+	$c->stash(alphabet_array => \@alphabet_array);
+	$c->render(template => 'create_table');
+};
+
+post '/create_table' => sub ($c) {
+	my $cols = $c->param('columns');
+	my $rows = $c->param('rows');
+	if (!($cols and $rows)) {
+		return $c->render(text => "No, this is broken, I think. Cols: $cols, Rows: $rows");
+	}
+	if (! (($cols =~ m/^\d+$/) and ($rows =~ m/^\d+$/)) ) {
+		return $c->render(text => "No, this is broken, I think. Cols: $cols, Rows: $rows");
+	}
+	
+	$c->stash(cols => $cols);
+	$c->stash(rows => $rows);
+	$c->stash(alphabet_array => \@alphabet_array);
+	$c->render(template => 'create_table');
+};
+
+post '/short' => sub ($c) {
+	my $long_boi = %{ from_json($c->req->body)}{url}; # wuh..? what about errors.. ? # dgafs lmfao
+	my $short = md5_sum $long_boi;
+	$c->render(json => {'data' => {'short' => "https://$ENV{TOKEN_HOSTNAME}/sh/" . $short}});
+	$cache->set($short => $long_boi);
+	# now to create persistant short -> long_boi ...
+	open(my $fh, '>>', './short.txt') or die;
+	# TODO gzip this
+	say $fh "$short => $long_boi";
+	close $fh;
+};
+
+get '/sh/:short' => sub ($c) {
+	if ($cache->get($c->param('short') )) {
+		return $c->redirect_to($cache->get( $c->param("short") ));
+	}
+	else {
+		# TODO un-gzip this
+		open(my $fh, '<', './short.txt') or return $c->render(text => "Sorry.");
+		while (my $line = <$fh>) {
+			my @line = split / => /, $line;
+			if ($line[0] eq $c->param('short')) {
+				chomp $line[1];
+				$c->redirect_to($line[1]);
+				last;
+			}
+		}
+		close $fh;
+	}
+};
+
+app->start;
+__DATA__
+
+@@ index.html.ep
+% layout 'default';
+% title 'Welcome';
+<h1>Welcome to the Mojolicious real-time web framework!</h1>
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html>
+  <head><title><%= title %></title></head>
+  <body><%= content %></body>
+</html>
